79c4a5fcc1cee6e318392f995df1c9e782cf3e20
DatHuynhCoder <htdat2004@gmail.com>
2025-03-11 17:52:54 +0700

    Set constant API_URL in product.js in store

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-const API_URL = process.env.API_URL || "http://localhost:5000"
+const API_URL = "https://productstore-i787.onrender.com/"
 
 export const useProductionStore = create((set) => ({
   products: [],
